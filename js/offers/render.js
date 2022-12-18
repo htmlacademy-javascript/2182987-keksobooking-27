ed6e95b20841f8a174db26928ebb1c8cae16d076
Alexandr Dragan <alexandr.dragan@sirenltd.com>
2022-12-18 16:35:52 +0200

    Комментарии и условия отображения

--- js/offers/render.js
+++ js/offers/render.js
@@ -6,16 +6,24 @@ const map = document.querySelector('#map-canvas');
 const renderOffers = () => {
   generateRealEstates(OBJECTS_QUANTITY).forEach((realEstate) => {
     const popup = popupTemplate.cloneNode(true);
+    // Добавление информации для простых полей
     popup.querySelector('.popup__title').textContent = realEstate.offer.title;
     popup.querySelector('.popup__text--address').textContent = realEstate.offer.address;
     popup.querySelector('.popup__text--price').textContent = `${realEstate.offer.price} ₽/ночь`;
     popup.querySelector('.popup__type').textContent = OfferTypes[(realEstate.offer.type).toUpperCase()];
     popup.querySelector('.popup__text--capacity').textContent = `${realEstate.offer.rooms} комнаты для ${realEstate.offer.guests} гостей`;
     popup.querySelector('.popup__text--time').textContent = `Заезд после ${realEstate.offer.checkin}, выезд до ${realEstate.offer.checkout}`;
-    popup.querySelector('.popup__description').textContent = realEstate.offer.description;
+    // Описание
+    if(realEstate.offer.description.length) {
+      popup.querySelector('.popup__description').textContent = realEstate.offer.description;
+    } else {
+      popup.querySelector('.popup__description').classList.add('hidden');
+    }
+    // Дополнительные услуги
     realEstate.offer.features.forEach((feature) => {
       popup.querySelector(`.popup__feature--${feature}`).classList.remove('hidden');
     });
+    // Фотографии
     const popupImgTemplate = popup.querySelector('.popup__photo');
     realEstate.offer.photos.forEach((photo) => {
       const popupImg = popupImgTemplate.cloneNode(true);
@@ -23,7 +31,7 @@ const renderOffers = () => {
       popup.querySelector('.popup__photos').append(popupImg);
     });
     popupImgTemplate.remove();
-
+    // Аватар
     popup.querySelector('.popup__avatar').src = realEstate.author.avatar;
     map.append(popup);
   });
